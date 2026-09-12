01d185cfc59c4ac99e2bd449089b30a659fac622
mthan <me@mthan.dev>
2026-09-12 17:07:32 +0300

    Initial Commit

---
+++ diynamic_playing/diynamic_playingApp.swift
@@ -0,0 +1,17 @@
+//
+//  diynamic_playingApp.swift
+//  diynamic_playing
+//
+//  Created by Muhammed on 12.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct diynamic_playingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
